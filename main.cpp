0499fcdd8d665af668b7eb3c2c89d1a6270d3cd6
Evgenii.Korchagov <joedraiser@gmail.com>
2021-10-18 09:22:31 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+void swap(int* a, int* b)
+{
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+int main()
+{
+    int a = 10;
+
+    int b = 20;
+
+    swap(&a, &b);
+
+    std::cout << a << " "  << b;
+    return 0;
+}
